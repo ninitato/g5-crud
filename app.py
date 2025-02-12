2ee4a47a6e74f594a6d2669a0c210443ff777a33
ninitato <nikkofortunado@gmail.com>
2025-02-12 11:25:52 +0800

    flask 1st commit

---
+++ app.py
@@ -0,0 +1,43 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define the model
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+# Create the database
+with app.app_context():
+    db.create_all()
+
+# Routes
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        
+        new_item = Item(name=name, description=description)
+        db.session.add(new_item)
+        db.session.commit()
+        
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
